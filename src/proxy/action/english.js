f0bbe7db4f250418688445495c415e87b3b60650
Andres M. Prieto <andresprieto2703@gmail.com>
2023-08-17 11:44:25 -0500

    Animation
    Fixing animation

--- src/proxy/action/english.js
+++ src/proxy/action/english.js
@@ -1,31 +1,34 @@
 const generateAction = (target, outputHelper) => {
   const englishActions = {
     bounce: () => {
-      target.classList = "animate__animated animate__bounce animate__infinite";
+      (target.classList = "animate__animated"),
+        "animate__bounce animate__infinite ball";
       outputHelper.innerText = "Bounce bounce bounce!";
     },
     zoom: () => {
-      target.classList = "animate__animated animate__zoomIn animate__infinite";
+      target.classList =
+        "animate__animated animate__zoomIn animate__infinite ball";
       outputHelper.innerText = "Look at me!";
     },
     tada: () => {
-      target.classList = "animate__animated animate__tada animate__infinite";
+      target.classList =
+        "animate__animated animate__tada animate__infinite ball";
       outputHelper.innerText = "Say hello to my little friend!!";
     },
     out: () => {
-      target.classList = "animate__animated animate__bounceOutUp";
+      target.classList = "animate__animated animate__bounceOutUp ball";
       outputHelper.innerText = "Yes, master";
     },
     come: () => {
-      target.classList = "animate__animated animate__bounceInUp";
+      target.classList = "animate__animated animate__bounceInUp ball";
       outputHelper.innerText = "Going lord";
     },
     ninja: () => {
-      target.classList = "animate__animated animate__flipOutY";
+      target.classList = "animate__animated animate__flipOutY ball";
       outputHelper.innerText = "In darkness we trust";
     },
     clear: () => {
-      target.classList = "";
+      target.classList = "floating ball";
       outputHelper.innerText = "* Cleaning ball *";
     },
   };
--- src/proxy/action/spanish.js
+++ src/proxy/action/spanish.js
@@ -1,31 +1,34 @@
 const generateAction = (target, outputHelper) => {
   const spanishActions = {
     rebota: () => {
-      target.classList = "animate__animated animate__bounce animate__infinite";
+      target.classList =
+        "animate__animated animate__bounce animate__infinite ball";
       outputHelper.innerText = "¡¡Reboto como pelota loca!!";
     },
     salta: () => {
-      target.classList = "animate__animated animate__zoomIn animate__infinite";
+      target.classList =
+        "animate__animated animate__zoomIn animate__infinite ball";
       outputHelper.innerText = "Voy y vuelvo, voy y vuelvo";
     },
     sorpresa: () => {
-      target.classList = "animate__animated animate__tada animate__infinite";
+      target.classList =
+        "animate__animated animate__tada animate__infinite ball";
       outputHelper.innerText = "¡Sorpresa!";
     },
     largo: () => {
-      target.classList = "animate__animated animate__bounceOutUp";
+      target.classList = "animate__animated animate__bounceOutUp ball";
       outputHelper.innerText = "Si, amo";
     },
     regresa: () => {
-      target.classList = "animate__animated animate__bounceInUp";
+      target.classList = "animate__animated animate__bounceInUp ball";
       outputHelper.innerText = "Si, amo";
     },
     sigilo: () => {
-      target.classList = "animate__animated animate__flipOutY";
+      target.classList = "animate__animated animate__flipOutY ball";
       outputHelper.innerText = "Nada es verdad todo está permitido";
     },
     limpia: () => {
-      target.classList = "";
+      target.classList = "floating ball";
     },
   };
   return spanishActions;
